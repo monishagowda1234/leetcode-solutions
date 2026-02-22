c77421dca43d3b642b7563d55a13ed6b41b701c2
Monisha Ashwath Gowda <134038996+monishagowda1234@users.noreply.github.com>
2026-02-22 11:05:59 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.4%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.py
@@ -0,0 +1,15 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        max_dist = 0
+        last_pos = -1
+        current_pos = 0
+        
+        while n > 0:
+            if n & 1:
+                if last_pos != -1:
+                    max_dist = max(max_dist, current_pos - last_pos)
+                last_pos = current_pos
+            n >>= 1
+            current_pos += 1
+            
+        return max_dist
