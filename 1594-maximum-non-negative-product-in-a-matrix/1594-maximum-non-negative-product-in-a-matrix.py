6f6e86cb50cbf8e87824bec32a9f0fdc1bc92a50
Monisha Ashwath Gowda <134038996+monishagowda1234@users.noreply.github.com>
2026-03-23 19:14:54 +0530

    Time: 3 ms (85.09%), Space: 19.4 MB (88.6%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -1,39 +1,60 @@
 class Solution:
-    def maxProductPath(self, grid):
-        m, n = len(grid), len(grid[0])
+    def maxProductPath(self, grid: List[List[int]]) -> int:
         MOD = 10**9 + 7
+        n = len(grid)
+        m = len(grid[0])
+
+        mn = [[0] * m for _ in range(n)]
+        mx = [[0] * m for _ in range(n)]
+        mn[0][0], mx[0][0] = grid[0][0], grid[0][0]
+
+        for i in range(n):
+            for j in range(m):
+                if i == 0 and j == 0: continue
+                if i == 0:
+                    mn[i][j] = mn[i][j - 1] * grid[i][j]
+                    mx[i][j] = mx[i][j - 1] * grid[i][j]
+                elif j == 0:
+                    mn[i][j] = mn[i - 1][j] * grid[i][j]
+                    mx[i][j] = mx[i - 1][j] * grid[i][j]
+                else:
+                    a = mn[i - 1][j] * grid[i][j]
+                    b = mx[i - 1][j] * grid[i][j]
+                    c = mn[i][j - 1] * grid[i][j]
+                    d = mx[i][j - 1] * grid[i][j]
+                    mn[i][j] = min(a, b, c, d)
+                    mx[i][j] = max(a, b, c, d)
         
-        max_dp = [[0]*n for _ in range(m)]
-        min_dp = [[0]*n for _ in range(m)]
-        
-        max_dp[0][0] = grid[0][0]
-        min_dp[0][0] = grid[0][0]
-        
-        # First column
-        for i in range(1, m):
-            max_dp[i][0] = max_dp[i-1][0] * grid[i][0]
-            min_dp[i][0] = max_dp[i][0]
-        
-        # First row
-        for j in range(1, n):
-            max_dp[0][j] = max_dp[0][j-1] * grid[0][j]
-            min_dp[0][j] = max_dp[0][j]
-        
-        # Fill DP
-        for i in range(1, m):
-            for j in range(1, n):
-                val = grid[i][j]
-                
-                a = max_dp[i-1][j] * val
-                b = min_dp[i-1][j] * val
-                c = max_dp[i][j-1] * val
-                d = min_dp[i][j-1] * val
-                
-                max_dp[i][j] = max(a, b, c, d)
-                min_dp[i][j] = min(a, b, c, d)
-        
-        result = max_dp[m-1][n-1]
-        
-        if result < 0:
-            return -1
-        return result % MOD
+        res = mx[-1][-1]
+        return res % MOD if res >= 0 else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
